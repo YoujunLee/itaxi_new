f9162df1089b605fb5c2163071e53b68c952972c
YoujunLee <injxyj@gmail.com>
2018-03-21 21:47:25 +0900

    전체 방정보 내려주는  php 파일에 방별 인원수 포함

--- assets/php/print_room.php
+++ assets/php/print_room.php
@@ -2,20 +2,31 @@
 
 <?php
 header('Access-Control-Allow-Origin: *');
- require_once './db.php';
+require_once './db.php';
+
+ $dbdata= array();
+
  $db = new DBC;
  $db->DBI();
  $db->query = "SELECT * FROM room";
  $db->DBQ();
- $num = $db->result->num_rows;
- 
- $dbdata= array();
- 
+
+ $db2 = new DBC;
+ $db2->DBI();
+
  while($data = $db->result->fetch_row()){
+    $db2->query = "SELECT * FROM room_user where post_id='".$data[0]."'";
+    $db2->DBQ();
+    $num = $db2->result->num_rows;
+
+    array_push($data, $num);
+
     $dbdata[]=$data;
  }
 
 echo json_encode($dbdata);
 
+$db->DBO();
+$db2->DBO();
 ?>
 
