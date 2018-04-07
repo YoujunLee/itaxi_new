6def834867b5d26edc0a48b365872eccda8f43e1
YoujunLee <injxyj@gmail.com>
2018-04-08 00:50:38 +0900

    방 생성 시, 방 참여도 함께 되도록 수정

--- assets/php/make_room.php
+++ assets/php/make_room.php
@@ -16,19 +16,36 @@
  else
      $post_id=1;
 
- $room_start = $_POST['departure'];
- $room_arrive = $_POST['arrival'];
- $room_date = $_POST['date'];
- $room_time = $_POST['time'];
- $room_population = $_POST['population'];
- $stu_id = '123';
-
+$room_start = $_POST['departure'];
+$room_arrive = $_POST['arrival'];
+$room_date = $_POST['date'];
+$room_time = $_POST['time'];
+$room_population = $_POST['population']; 
 $room_date=strtotime($room_date);
 
+$stu_id=$_COOKIE['stu_id'];
+$phone=$_COOKIE['phone'];
+$mail=$_COOKIE['mail'];
+
+ /*room user table id 값 확인 */
+ $db2 = new DBC; 
+ $db2->DBI();
+ $db2->query = "SELECT id FROM room_user ORDER BY id desc LIMIT 1"; 
+ $db2->DBQ();
+ $num1 = $db2->result->num_rows;
+ $data1 = $db2->result->fetch_row();
+ if($num1==1)
+   $id = $data1[0]+1;	
+ else
+   $id=1;
+
 /*방 생성하기*/
- $db->query = "INSERT INTO room VALUES ('".$post_id ."','".$stu_id."','".$room_start."', '".$room_arrive."','".$room_date."','".$room_time."','".$room_population."')";
+ $db->query = "INSERT INTO room VALUES ('".$post_id ."','".$room_start."', '".$room_arrive."','".$room_date."','".$room_time."','".$room_population."')";
  $db->DBQ();
 
+ $db->query = "INSERT INTO room_user VALUES ('".$id."','".$post_id."','".$stu_id."','".$mail."','".$phone."')";
+ $db->DBQ();
+ 
  if(!$db->result){
      echo "<script>alert('fail to posting.');</script>";
      $db->DBO();
