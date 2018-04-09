8c738da1a5b7f8d9a59eaf5bca1c49c3b7dc4c25
YoujunLee <injxyj@gmail.com>
2018-04-09 23:29:48 +0900

    메일 디버깅 echo 제거

--- assets/php/enter_room.php
+++ assets/php/enter_room.php
@@ -54,11 +54,10 @@
  $data2 = $db2->result->fetch_row();
  $leader_mail = $data2[0];
 
- echo $leader_mail;
  $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
  try {
     //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
