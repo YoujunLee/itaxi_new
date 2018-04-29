ba9654a0cc554a7558448f0479d1dc8b0b7e9b53
YoujunLee <injxyj@gmail.com>
2018-04-29 21:10:19 +0900

    방 내부 영문화

--- assets/js/room_json.js
+++ assets/js/room_json.js
@@ -8,7 +8,7 @@ $.urlParam = function(name){
 }
 
 $(document).ready(function () {
-myurl='http://52.78.208.153/assets/php/get_room.php?post_id='+$.urlParam('post_id');
+myurl='./assets/php/get_room.php?post_id='+$.urlParam('post_id');
    $.ajax({
        dataType: "json",
        url: myurl,
@@ -26,25 +26,25 @@ myurl='http://52.78.208.153/assets/php/get_room.php?post_id='+$.urlParam('post_i
         var tr_date=$("<tr>").appendTo(table);
         var th_date=$("<th scopte='row'>").appendTo(tr_date);
         var td_date=$("<td>").appendTo(tr_date);
-        th_date.html("날짜");
+        th_date.html("Date");
         td_date.html(data[0]['date']);
         
         var tr_time=$("<tr>").appendTo(table);
         var th_time=$("<th scopte='row'>").appendTo(tr_time);
         var td_time=$("<td>").appendTo(tr_time);
-        th_time.html("시간");
+        th_time.html("Time");
         td_time.html(data[0]['time']);
 
         var tr_departure=$("<tr>").appendTo(table);
         var th_departure=$("<th scopte='row'>").appendTo(tr_departure);
         var td_departure=$("<td>").appendTo(tr_departure);
-        th_departure.html("출발지");
+        th_departure.html("Departure");
         td_departure.html(data[0]['departure']);
 
         var tr_arrival=$("<tr>").appendTo(table);
         var th_arrival=$("<th scopte='row'>").appendTo(tr_arrival);
         var td_arrival=$("<td>").appendTo(tr_arrival);
-        th_arrival.html("도착지");
+        th_arrival.html("Arrival");
         td_arrival.html(data[0]['arrival']);
 
         var participant=$('#participant');
