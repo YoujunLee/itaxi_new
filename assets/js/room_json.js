aaaf5840329d2c6cae7d97ea6435cfd36ab4165e
YoujunLee <injxyj@gmail.com>
2018-08-04 16:43:08 +0900

    핸드폰번호 전화/sms 모바일 분기 완료

--- assets/js/room_json.js
+++ assets/js/room_json.js
@@ -67,10 +67,11 @@ myurl='./assets/php/get_room.php?post_id='+$.urlParam('post_id');
         var td_no=$('<td>').appendTo(tr_tbody);
         var td_id=$('<td>').appendTo(tr_tbody);
         var td_num=$('<td>').appendTo(tr_tbody);
-
+           
         td_no.html(index);
         td_id.html(data[1][index][1]);
-        td_num.html(data[1][index][3]);
+        td_num.html('<span class="phone_num">'+data[1][index][3]+'</span><a class="material-icons sms_num" href="tel:'+data[1][index][3]+'">call</a>&nbsp;&nbsp;&nbsp;<a class="material-icons sms_num" href="sms:'+data[1][index][3]+'">sms</a>');
+
         });
         }
     });
